f46b76262c750be91a22656ed8b0a2c27081c2d6
nickbild <Nicholas.Bild@sprint.com>
2019-08-02 12:03:53 -0400

    Update for Python3 compatibility.

--- capture_images.py
+++ capture_images.py
@@ -21,13 +21,13 @@ def gstreamer_pipeline (capture_width=3280, capture_height=2464, display_width=i
 
 
 # Capture [num_images] images, spaced [delay_sec] seconds apart.
-def save_frame_sequence(num_images, delay_sec):
+def save_frame_sequence(num_images, delay_sec, cap):
     if cap.isOpened():
         for i in range(num_images+20):
             ret_val, img = cap.read()
             if i >= 20: # Skip first 20 to let camera get lighting straigtened out.
-                cv2.imwrite(dir + "/img_" + str(i) + "_" + batch + ".jpg", img)
-            sleep(delay_sec)
+                cv2.imwrite(dir + "/img_" + batch + "_" + str(i) + ".jpg", img)
+                sleep(delay_sec)
 
         cap.release()
     else:
@@ -38,4 +38,4 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     sleep(5)
     print("Go!")
-    save_frame_sequence(10, 0.2)
+    save_frame_sequence(10, 0.2, cap)
